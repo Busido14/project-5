e9cb778ee0effcfe718b8665403ca490f189f40e
Artem <artchzhen@gmail.com>
2022-12-26 08:40:47 +0500

    init devol

---
+++ project 5/project 5/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  project 5
+//
+//  Created by Артем Чжен on 26/12/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var allWords = [String]()
+    var usedWords = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+    }
+
+
+}
+
